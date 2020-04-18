80a4ab77f05422f9bed3b07fb48a1d240dbe0d79
Shamsullo <shamsulloismatov@gmail.com>
2020-04-18 11:26:58 +0600

    working on the translation

---
+++ enactus/the_app/translation.py
@@ -0,0 +1,17 @@
+
+from modeltranslation.translator import register, TranslationOptions
+from .models import AboutEnactus, Projects, AcademicAdvisors
+
+@register(AboutEnactus)
+class AboutEnactusTranslationOptions(TranslationOptions):
+    fields = ('title', 'text')
+
+
+@register(Projects)
+class ProjectsTranslationOptions(TranslationOptions):
+    fields = ('name', 'description')
+
+
+@register(AcademicAdvisors)
+class AcademicAdvisorsTranslationOptions(TranslationOptions):
+    fields = ('f_name', 'l_name', 'position', 'desc')
